f35d534406a75d964b82a88e8443f6a50bdde6cb
fonsirada <fonsirada13@gmail.com>
2024-08-26 00:41:48 -0400

    worked on efficiency, pretty much where i want it to be

--- wschatbot.py
+++ wschatbot.py
@@ -2,266 +2,89 @@ import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 
+# keeps track of program state
 user = None
 
-def find_year():
-    year = ""
-    while not year.isdigit():
-        user_input = input("Please enter a year (i.e: 1977) between 1903 and 2023 (no spaces!): ")
-        if user_input.isdigit():
-            year = int(user_input)
-            break
-        else:
-            print("That is not a valid year.")
-    special_case = check_year(year)
-    #formula for finding the row that corresponds to each year
-    row = 124 - (year - 1899)
-    return year, row, special_case
-        
-def check_year(year):
-    #special cases
-    if (year > 2023 or year < 1903 or year == 1994 or year == 1904):
-        #user entered a year that has not happened yet
-        if year > 2023:
-            print("I am afraid I cannot predict the future.")
-        #user entered a year that is before 1903
-        elif year < 1903:
-            print("I am afraid I cannot find information before 1903.")
-        #years where there was no world series
-        elif year == 1994:
-            print("There was no World Series held in 1994 due to The Players' Strike")
-        elif year == 1904:
-            print("There was no World Series held in 1904 due to the boycott by the New York Giants")
-        return True
-    else:
-        return False
+####### CHOICE FUNCTIONS #######
 
-def qA(tbody):
-    year, row, special_case = find_year()
-    if special_case:
-        return
-    trs = tbody.find_all("tr")
-    tds = trs[row].find_all("td")
-    
-    #index 0 is american league winner index 3 is national league winner
-    #the winner is bolded on the html
-    if tds[0].find("strong") != None:
+def handle_question(tbody, question):
+    if question in {"b", "c", "d", "e"}:
+        data = parse_table(tbody, question)
+        plot_graph(data, question)
+    else:
+        year, row, special_case = find_year()
+        if special_case:
+            return
+        tds = tbody.find_all("tr")[row].find_all("td")
+        winner, loser = get_winner_loser(tds)
+        print(f'In {year}, the {winner} won the World Series against the {loser}.')
+
+        if question == "f":
+            team, manager = find_manager(tds, winner, loser)
+            print(f'The manager of the {year} {team} was {manager}.')
+
+####### SCRAPING FUNCTIONS #######
+
+# function that finds the winner and loser of a world series matchup
+def get_winner_loser(tds):
+    # index 0 is american league team, index 3 is national league team
+    # winner is bolded on the html
+    if tds[0].find("strong") is not None:
         winner = tds[0].find("strong").text
         loser = tds[3].text
-    elif tds[3].find("strong") != None:
+    else:
         winner = tds[3].find("strong").text
         loser = tds[0].text
-    
-    print(f'In {year}, the {winner} won the World Series against the {loser}.')
-        
-def qB(tbody):
-    trs = tbody.find_all("tr")
-    winners = {}
-    for index in range(len(trs)): 
-        #individual tds within each tr
-        indiv_tds = trs[index].find_all("td")
-        ##only accounts for rows with world series info
-        if len(indiv_tds) >= 4:
-            #index 0 is american league winner index 3 is national league winner
-            #the world series winner is bolded on the website
-            if indiv_tds[0].find("strong") != None:
-                winner = indiv_tds[0].find("strong").text
-                if winner in winners:
-                    winners[winner] += 1
-                else:
-                    winners[winner] = 1
-            else:
-                winner = indiv_tds[3].find("strong").text
-                if winner in winners:
-                    winners[winner] += 1
-                else:
-                    winners[winner] = 1
-    keys = list(winners.keys())
-    values = list(winners.values())
-    fig, ax = plt.subplots()
-    hbars = ax.barh(keys, values, align='center')
-    ax.set_yticks(keys, labels=keys, fontsize=6)
-    ax.invert_yaxis()
-    ax.set_xlabel('World Series Wins')
-    ax.set_title('World Series Wins by Team')
-    ax.bar_label(hbars)
-    ax.set_xlim(right=30)
-    print("I have produced a bar graph that highlights the number of World Series' wins by team.")
-    print("The New York Yankees have won the most World Series' with 27 wins.")
-    plt.show()
-    
-def qC(tbody):
-    trs = tbody.find_all("tr")
-    losers = {}
-    for index in range(len(trs)): 
-        #individual tds within each tr
-        indiv_tds = trs[index].find_all("td")
-        ##only accounts for rows with world series info
-        if len(indiv_tds) >= 4:
-            #index 0 is american league winner index 3 is national league winner
-            #the world series winner is bolded on the website
-            if indiv_tds[0].find("strong") != None:
-                loser = indiv_tds[3].text
-                if loser in losers:
-                    losers[loser] += 1
-                else:
-                    losers[loser] = 1
-            else:
-                loser = indiv_tds[0].text
-                if loser in losers:
-                    losers[loser] += 1
-                else:
-                    losers[loser] = 1
-    keys = list(losers.keys())
-    values = list(losers.values())
-    fig, ax = plt.subplots()
-    hbars = ax.barh(keys, values, align='center')
-    ax.set_yticks(keys, labels=keys, fontsize=6)
-    ax.invert_yaxis()
-    ax.set_xlabel('World Series Losses')
-    ax.set_title('World Series Losses by Team')
-    ax.bar_label(hbars)
-    ax.set_xlim(right=15)
-    print("I have produced a bar graph that highlights the number of World Series' losses by team.")
-    print("The New York Yankees have lost the most World Series' with 13 losses.")
-    plt.show()
+    return winner, loser
 
-def qD(tbody):
+# function that parses the data table based on user choice
+def parse_table(tbody, question):
     trs = tbody.find_all("tr")
-    appearances = {}
-    for index in range(len(trs)): 
-        #individual tds within each tr
-        indiv_tds = trs[index].find_all("td")
-        ##only accounts for rows with world series info
-        if len(indiv_tds) >= 4:
-            #index 0 is american league winner index 3 is national league winner
-            #the world series winner is bolded on the website
-            if indiv_tds[0].find("strong") != None:
-                team = indiv_tds[0].find("strong").text
-                if team in appearances:
-                    appearances[team] += 1
-                else:
-                    appearances[team] = 1
-                team = indiv_tds[3].text
-                if team in appearances:
-                    appearances[team] += 1
-                else:
-                    appearances[team] = 1
-            else:
-                team = indiv_tds[3].find("strong").text
-                if team in appearances:
-                    appearances[team] += 1
-                else:
-                    appearances[team] = 1
-                team = indiv_tds[0].text
-                if team in appearances:
-                    appearances[team] += 1
-                else:
-                    appearances[team] = 1
-    keys = list(appearances.keys())
-    values = list(appearances.values())
-    fig, ax = plt.subplots()
-    hbars = ax.barh(keys, values, align='center')
-    ax.set_yticks(keys, labels=keys, fontsize=6)
-    ax.invert_yaxis()
-    ax.set_xlabel('World Series Appearances')
-    ax.set_title('World Series Appearances by Team')
-    ax.bar_label(hbars)
-    ax.set_xlim(right=42)
-    print("I have produced a bar graph that highlights the number of World Series' appearances by team.")
-    print("The New York Yankees have the most World Series' apperarances with 40.")
-    plt.show()
-    
-def qE(tbody):
-    trs = tbody.find_all("tr")
-    wins = {}
-    for index in range(len(trs)): 
-        #individual tds within each tr
-        indiv_tds = trs[index].find_all("td")
-        ##only accounts for rows with world series info
-        if len(indiv_tds) >= 4:
-            #index 0 is american league winner index 3 is national league winner
-            #the world series winner is bolded on the website
-            if indiv_tds[0].find("strong") != None:
-                american = "American League"
-                if american in wins:
-                    wins[american] += 1
-                else:
-                    wins[american] = 1
-            else:
-                national = "National League"
-                if national in wins:
-                    wins[national] += 1
-                else:
-                    wins[national] = 1
-    plt.title("World Series Wins by League")
-    plt.pie(wins.values(), labels=wins.keys(), autopct='%.0f%%', pctdistance=1.15, labeldistance=.25)
-    print("I have produced a pie chart that highlights World Series' wins by league.")
-    print("The American League has won more World Series' than the National League.")
-    plt.show()
+    data = {}
+    for tr in trs:
+        tds = tr.find_all("td")
+        if len(tds) >= 4:
+            winner, loser = get_winner_loser(tds)
+            if question == "b":
+                update_counts(data, winner)
+            elif question == "c":
+                update_counts(data, loser)
+            elif question == "d":
+                update_counts(data, winner)
+                update_counts(data, loser)
+            elif question == "e":
+                update_counts(data, "American League" if winner in tds[0].text else "National League")
+    return data
 
-# choice F - manager of a world series team
-def qF(tbody):
-    year, row, special_case = find_year()
-    if special_case:
-        return
-    trs = tbody.find_all("tr")
-    tds = trs[row].find_all("td")
-    
-    #index 0 is american league winner index 3 is national league winner
-    #the winner is bolded on the html
-    if tds[0].find("strong") != None:
-        winner = tds[0].find("strong").text
-        loser = tds[3].text
-    elif tds[3].find("strong") != None:
-        winner = tds[3].find("strong").text
-        loser = tds[0].text
-    
-    print(f'In {year}, the {winner} won the World Series against the {loser}.')
-    team, manager = find_manager(tds, winner, loser)
-    print(f'The manager of the {year} {team} was {manager}.')
-
-# choice F_a - manager of winning world series team
-def qF_a(tds):
-    #index 0 is american league winner index 3 is national league winner
-    #the world series winner is bolded on the html
-    if tds[0].find("strong") != None:
-        team = tds[0].find("strong").text
-        a = tds[0].find("a")
-        link = a['href']
+# function that updates dictionary passed as an argument
+def update_counts(dictionary, key):
+    if key in dictionary:
+        dictionary[key] += 1
     else:
-        team = tds[3].find("strong").text
-        a = tds[3].find("a")
-        link = a['href']
-    return team, a, link
+        dictionary[key] = 1
 
-#choice F_b - manager of losing world series team
-def qF_b(tds):
-    #index 0 is american league winner index 3 is national league winner
-    #the world series loser is not bolded on the html
-    if tds[0].find("strong") != None:
-        team = tds[3].text
-        a = tds[3].find("a")
-        link = a['href']
-    else:
-        team = tds[0].text
-        a = tds[0].find("a")
-        link = a['href']
-    return team, a, link
-
-#find manager function
+# finds manager based on user input
 def find_manager(tds, winner, loser):
     print(f'Do you want to find the manager for the {winner} or the {loser}?')
     print(f'   a) {winner}')
     print(f'   b) {loser}')
-    options = {
-        "a": qF_a,
-        "b": qF_b
-    }
-    choice = getUserChoice(options)
-    team, a, link = options[choice](tds)
+    choice = getUserChoice({"a": "a", "b": "b"})
 
-    link = "https://www.baseball-reference.com" + link
+    if choice == "a":
+        team, link = (winner, get_manager_link(tds, 0 if tds[0].find("strong") else 3))
+    else:
+        team, link = (loser, get_manager_link(tds, 3 if tds[0].find("strong") else 0))
+
+    manager = scrape_manager(link)
+    return team, manager
+
+# gets link to find manager
+def get_manager_link(tds, index):
+    link = tds[index].find("a")['href']
+    return "https://www.baseball-reference.com" + link
+
+# scrapes link to find manager
+def scrape_manager(link):
     page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html.parser')
     body = soup.find("body")
@@ -272,7 +95,59 @@ def find_manager(tds, winner, loser):
     ps = div3.find_all("p")
     p = ps[2]
     a = p.find("a")
-    return team, a.text
+    return a.text
+
+####### GRAPHING FUNCTIONS #######
+
+# plots graphs based on question
+def plot_graph(data, question):
+    titles = {
+        "b": "World Series Wins by Team",
+        "c": "World Series Losses by Team",
+        "d": "World Series Appearances by Team",
+        "e": "World Series Wins by League"
+    }
+    xlabels = {
+        "b": "Wins",
+        "c": "Losses",
+        "d": "Appearances"
+    }
+    if question == "e":
+        plt.title(titles[question])
+        plt.pie(data.values(), labels=data.keys(), autopct='%.0f%%', pctdistance=1.15, labeldistance=.25)
+        print("I have produced a pie chart that highlights World Series' wins by league.")
+        print("The American League has won more World Series' than the National League.")
+        plt.show()
+    else:
+        print_statements(question)
+        plot_bargraph(data, titles[question], xlabels[question])
+
+# prints statements related to graphs
+def print_statements(question):
+    if question == 'b':
+        print("I have produced a bar graph that highlights the number of World Series' wins by team.")
+        print("The New York Yankees have won the most World Series' with 27 wins.")
+    elif question == 'c':
+        print("I have produced a bar graph that highlights the number of World Series' losses by team.")
+        print("The New York Yankees have lost the most World Series' with 13 losses.")
+    else:
+        print("I have produced a bar graph that highlights the number of World Series' appearances by team.")
+        print("The New York Yankees have the most World Series' apperarances with 40.")
+
+# prints bargraph based on question
+def plot_bargraph(data, title, xlabel):
+    keys = list(data.keys())
+    values = list(data.values())
+    fig, ax = plt.subplots()
+    hbars = ax.barh(keys, values, align='center')
+    ax.set_yticks(keys, labels=keys, fontsize=6)
+    ax.invert_yaxis()
+    ax.set_xlabel(xlabel)
+    ax.set_title(title)
+    ax.bar_label(hbars)
+    plt.show()
+
+####### USER INTERACTION FUNCTIONS #######
 
 # lists choices and prompts user to enter choice
 def options():
@@ -285,18 +160,10 @@ def options():
     print("   f) Manager of A World Series Team")
     return getUserChoice()
 
-# dictionary storing choices and functions that correspond to each choice
-questions = {
-    "a": qA,
-    "b": qB,
-    "c": qC,
-    "d": qD,
-    "e": qE,
-    "f": qF
-}
-
 # prompts user to enter choice, will continue until choice is valid
-def getUserChoice(options = questions):
+def getUserChoice(options = None):
+    if not options:
+        options = {"a", "b", "c", "d", "e", "f"}
     choice = None
     while choice not in options:
         choice = input("Please enter the letter of your choice: ").lower()
@@ -304,12 +171,30 @@ def getUserChoice(options = questions):
             print("That is not one of the letters available.")
     return choice
 
-# chatbot introduction and grabs user name
-def intro():
-    user = input("Hello! Please enter your name: ")
-    print(f'Nice to meet you {user}, my name is BaseballBot, and I love baseball!')
-    print("I see that you are interested in scraping Baseball Reference for World Series Data.")
-    return user
+# gets row that has data for corresponding year
+def find_year():
+    # ensures that a valid year is inputted
+    while True:
+        year = input("Please enter a year (i.e: 1977) between 1903 and 2023 (no spaces!): ")
+        if year.isdigit() and 1903 <= int(year) <= 2023:
+            year = int(year)
+            break
+        print("That is not a valid year.")
+    special_case = check_year(year)
+    #formula for finding the row that corresponds to each year
+    row = 124 - (year - 1899)
+    return year, row, special_case
+
+# checks if the year is a special case where there was no world series that year
+def check_year(year):
+    if year == 1994:
+        print("There was no World Series held in 1994 due to The Players' Strike")
+        return True
+    elif year == 1904:
+        print("There was no World Series held in 1904 due to the boycott by the New York Giants")
+        return True
+    else:
+        return False
 
 # asks user if they want to find something else
 def another_question(user):
@@ -320,12 +205,18 @@ def another_question(user):
     if answer in affirmative:
         main()
     elif answer in negative:
-        print("Alright. Thanks for chatting with me!")
+        print("Ok! I hope you learned something new. Thanks for chatting with me!")
     else:
         print("I don't understand...")
         another_question(user)
 
-# main function
+# chatbot introduction and grabs user name
+def intro():
+    user = input("Hello! Please enter your name: ")
+    print(f'Nice to meet you {user}. My name is BaseballBot, and I scrape Baseball Reference for World Series Data.')
+    return user
+
+####### MAIN FUNCTION #######
 def main():
     global user
     page = requests.get("https://www.baseball-reference.com/postseason/world-series.shtml")
@@ -337,15 +228,14 @@ def main():
     table = div2.find("table", class_="sortable stats_table")
     tbody = table.find("tbody")
     
-    #introduction
+    #introduction - only if user hasnt been created yet (initial start up)
     if not user:
         user = intro()
 
-    #list choices for scraping and carry out functions
+    #list choices for scraping and carry out user choice
     choice = options()
-    if choice in questions:
-        questions[choice](tbody)
-        another_question(user)
+    handle_question(tbody, choice)
+    another_question(user)
     return
     
 if __name__ == "__main__":
